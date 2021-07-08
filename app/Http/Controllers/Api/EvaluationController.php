e2448874fbc318aad89d75c1116cb383f7cd66d2
marcos murilo <marcosmeschial@gmail.com>
2021-07-08 12:29:24 -0300

    Criado index e store de evaluation

---
+++ app/Http/Controllers/Api/EvaluationController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreEvaluation;
+use App\Http\Resources\EvaluationResource;
+use App\Models\Evaluation;
+
+class EvaluationController extends Controller
+{
+    protected $repository;
+
+    public function __construct(Evaluation $model)
+    {
+        $this->repository = $model;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($company)
+    {
+        $evaluations = $this->repository->where('company', $company)->get();
+
+        return EvaluationResource::collection($evaluations);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreEvaluation $request, $company)
+    {
+        $evaluation = $this->repository->create($request->validated());
+
+        return new EvaluationResource($evaluation);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\EvaluationController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/evaluations/{company}', [EvaluationController::class, 'index']);
+Route::post('/evaluations/{company}', [EvaluationController::class, 'store']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return response()->json(['message' => 'success']);
 });
